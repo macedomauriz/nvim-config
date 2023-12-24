fe050cb39bfc252b7efa2542860edb03d24631f6
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2023-12-24 15:55:52 -0300

    bash lsp, formatting, linter

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,36 +1,38 @@
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup({
-	sources = {
-		formatting.prettierd,
-		formatting.stylua,
-		formatting.black,
-		diagnostics.eslint_d,
-		diagnostics.flake8,
-	},
-	lsp_formatting = function(bufnr)
-		vim.lsp.buf.format({
-			filter = function(client)
-				-- apply whatever logic you want (in this example, we'll only use null-ls)
-				return client.name == "null-ls"
-			end,
-			bufnr = bufnr,
-		})
-	end,
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-})
+null_ls.setup {
+  sources = {
+    formatting.prettierd,
+    formatting.stylua,
+    formatting.black,
+    formatting.beautysh,
+    diagnostics.eslint_d,
+    diagnostics.flake8,
+    diagnostics.shellcheck,
+  },
+  lsp_formatting = function(bufnr)
+    vim.lsp.buf.format {
+      filter = function(client)
+        -- apply whatever logic you want (in this example, we'll only use null-ls)
+        return client.name == "null-ls"
+      end,
+      bufnr = bufnr,
+    }
+  end,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
+}
